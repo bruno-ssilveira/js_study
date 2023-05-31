94789cab3e3f7256724a8b4a18bf8046b5e7ce33
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-05-31 14:28:30 -0300

    testes

---
+++ script.js
@@ -0,0 +1,18 @@
+//var
+
+//var altura = 5;
+//var comprimento = 7;
+//var area = altura * comprimento;
+//console.log(area)
+//var area;
+
+// var pode ser declarada a mesma várias vezes, o que pode dar bug por descuidado
+
+let forma = 'retângulo';
+let altura = 5;
+let comprimento = 7;
+
+area = altura * comprimento;
+console.log(area)
+
+// var let const
