aca53516f26b89a6517a529b559218891a35bb31
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-06-01 12:48:20 -0300

    var, let and const

---
+++ variables.js
@@ -0,0 +1,13 @@
+var respostaDeTudo = 42;
+let idade = 29;
+const pi = 3.14;
+idade = 30;
+
+{
+    var respostaDeTudo = 3.14;
+    let idade = null;
+    const pi = "bruno";
+    console.log(respostaDeTudo, idade, pi)
+}
+
+console.log(respostaDeTudo, idade, pi)
