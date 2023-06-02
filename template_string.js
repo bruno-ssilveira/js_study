c5699baffc2701329b22d9ba27cf960d4c9c3f24
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-06-01 22:39:55 -0300

    template string

---
+++ template_string.js
@@ -0,0 +1,11 @@
+//console.log("deu erro");
+//console.error(new Error("deu erro"));
+
+
+const nome = 'bruno';
+const idade = 23;
+const cidadeDeNascimento = 'Porto Alegre';
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
